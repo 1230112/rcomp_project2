52b145c6b1e90eaafd201e8a4effbafeccb33107
laFlorNegra <1230112@isep.ipp.pt>
2024-06-07 16:18:04 +0200

    constants

--- MessageCodes.java
+++ MessageCodes.java
@@ -28,38 +28,9 @@ public final class MessageCodes {
     /**
      * The constant CB.
      */
-    public static final int CB = 5;
-
-    /**
-     * The constant CPI.
-     */
-    public static final int CPI = 6;
-
-    /**
-     * The constant UPI.
-     */
-    public static final int UPI = 7;
-
-    /**
-     * The constant UPIC.
-     */
-    public static final int UPIC = 10;
-
-    /**
-     * The constant UPIP.
-     */
-    public static final int UPIP = 11;
-
-    /**
-     * The constant DPI.
-     */
-    public static final int DPI = 12;
-
-    public static final int VBH = 13;
-
-    public static final int RVBH = 14;
-
-    public static final int SAB = 15;
+    public static final int UPLOAD_FILE = 5;
+    public static final int DOWNLOAD_FILE = 6;
+    public static final int DELETE_FILE = 7;
 
     private MessageCodes() {
         // Private constructor to prevent instantiation
